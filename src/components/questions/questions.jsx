525e0895ef5dda4eec3f8861c38a95a7e69bf217
hertbex <mlive654@gmail.com>
2024-05-11 13:42:37 +0500

    sdelal zikl dalshe ne ebu

--- src/components/questions/questions.jsx
+++ src/components/questions/questions.jsx
@@ -2,17 +2,8 @@ import { Link } from 'react-router-dom'
 import './questions.css'
 import React, { useRef, useState } from "react"
 import store from '../../state/store'
-export const QuestionInput = ({obj, onAnswer}) =>{
+export const QuestionInput = ({obj, handleAnswerClick}) =>{
   const [buttonClass, setButtonClass] = useState('question-button')
-  const handleAnswerClick = () => {
-    onAnswer(obj.value);
-    if (obj.value === "yes"){
-      setButtonClass('question-button-right')
-    }
-    else{
-      setButtonClass('question-button-wrong')
-    }
-  }
   return(
     <div className='question-inputs' key={obj.key}>
       <button
@@ -22,7 +13,7 @@ export const QuestionInput = ({obj, onAnswer}) =>{
         onClick={handleAnswerClick}
 
       >
-        {obj.key} {buttonClass}
+        {obj.key}
       </button>
     </div>
   );
@@ -30,25 +21,27 @@ export const QuestionInput = ({obj, onAnswer}) =>{
 };
 
 export const Questions = () => {
-    // const rightAnswerCount = useRef(0)
-    // const [checked, setChecked] = useState(false)
-    // const onAnswer = (value) => {
-    //   if(value === 'yes'){
-    //     console.log(`current: ${rightAnswerCount.current + 1}`);
-    //     rightAnswerCount.current = rightAnswerCount.current + 1;
-    //   }
-    //   else{
-    //     console.log('wrong')
-    //   }
-    //   setChecked(true)
-    // }
+
     const rightAnswerCount = useRef(0);
-    const onAnswer = (value) => {
+    const onAnswer = (value, answers) => {
         if (value === 'yes') {
             rightAnswerCount.current = rightAnswerCount.current + 1;
             console.log(`Current right answer count: ${rightAnswerCount.current}`);
         } else {
             console.log('Wrong answer');
+
+        }
+        for (var i in answers){
+        const right = 'question-button-right'
+        const wrong = 'question-button-wrong'
+            if(answers[i].value === 'yes'){
+              // setButtonClass(right)
+              console.log(right)
+            }
+            else{
+              // setButtonClass(wrong)
+              console.log(wrong)
+            }
         }
     };
     return (
@@ -66,7 +59,8 @@ export const Questions = () => {
                   <QuestionInput
                     key={obj.key}
                     obj={obj}
-                    onAnswer={()=> onAnswer(obj.value)}
+                    handleAnswerClick={()=> onAnswer(obj.value, item.answers)}
+
                   />
                 )
                   
--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -1,12 +1,12 @@
 import './results.css'
 import React from "react";
-import { Route, Routes, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Questions } from '../questions/questions';
 export const Results = (props) => {
     return(
         <div className="result-wrapper">
             <h2 className='result-title'>ПРАВИЛЬНЫЕ ОТВЕТЫ</h2>
-            <div className='result-count'></div>
+            <div className='result-count'>{props.rightAnswerCount}</div>
 
             <Link to='/test-env/questions'><input className='result-button' type='button' value='RESTART'/></Link>
         </div>
