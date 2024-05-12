27912dda1575892256ddc26f8c3149caee81472e
hertbex <mlive654@gmail.com>
2024-05-12 21:13:08 +0500

    wdwdwdwdwdwdwd

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -5,7 +5,7 @@ export const Results = (props) => {
     return(
         <div className="result-wrapper">
             <h2 className='result-title'>ПРАВИЛЬНЫЕ ОТВЕТЫ</h2>
-            <div className='result-count'>{props.rightAnswerCount}</div>
+            <div className='result-count'>8</div>
 
             <Link to='/test-env/questions'><input className='result-button' type='button' value='RESTART'/></Link>
         </div>
